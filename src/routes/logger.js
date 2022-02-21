e78180f8c5500143748ccdd464e86ffc36a4a25c
rmashum <r.mashum@gmail.com>
2022-02-21 17:08:19 +0530

    update file

---
+++ src/routes/logger.js
@@ -0,0 +1,8 @@
+let url ='https://www.google.com/';
+
+function logg(message){
+    console.log(message)
+}
+
+module.exports.endpoint=url;
+module.exports.logg=logg;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+let obj =require('./logger')
 
 router.get('/test-me', function (req, res) {
+    console.log(obj.endpoint);
+    obj.logg('thorium');
     res.send('My first ever api!')
 });
 
