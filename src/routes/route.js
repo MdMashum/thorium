f5a4042cdf0ef559661d846b9dd29a1c0f973812
rmashum <r.mashum@gmail.com>
2022-02-24 21:46:22 +0530

    Post-api-queryparam

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,48 +1,135 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/students/:name', function(req, res) {
-//     let studentName = req.params.name
-//     console.log(studentName)
-//     res.send(studentName)
-// })
-router.get('/movies',function (req ,res) {
 
-    res.send('["fukrey","delhi","kapil","abul","mumbai"]')
-});
+// router.post('/players/:playerName/bookings/:bookingId', function (req, res) {
 
-// fetch movie by id
-  router.get('/movies/:movieId', function(req,res){
-      mov=["fukrey","delhi","kapil","abul","mumbai"]
-      let value=req.params.movieId;
-      if(value>mov.length-1){
-          res.send('"Not found"')
-      }
-      else {
-          res.send(mov[value])
-      }
-  })
+//     router.get("/get-query-1", function(req,res){
+//         let data =req.query
+//         console.log(data)
+//         res.send(data:data ,status :true})
+//         })
+        
+        
+//         // fail less than 40
+        
+//          router.get("/get-query-2", function(req,res){
+//          let marks=req.query.marks
+//         let result
+//          if (marks>40) {result = pass}
+//         else{
+//         result=fail
+//         }
+//          res.send(result: result,status :true})
+//          })
+        
+//         // optimize code
+        
+//          router.get("/get-query-2", function(req,res){
+//          let marks=req.query.marks
+//         let result =marks>40? "pass":"fail"
+//         //let result ="fail"
+//          if (marks>40) {result = pass}
+//          // else{result=fail}
+//          res.send(result: result,status :true})
+//          })
+        
+        
+        
+        
+//         //post request
+//         let myarr =[23,45,67,2838,3423,423,24,4,234,234]
+//         //filter out all greater than input is received
+//          router.post("/post-query-1", function(req,res){
+//         let input =req.query.input
+//         let finalArr =[]
+//         for(i=0;i<myArray.length;i++){
+//         if(myArr[i] > input ) finalArr.push(myArr[i])}
+//         }
+//          //let data=req.query
+//         //console.log(data)
+//          res.send(result: finalArr , status :true})
+//          })
+        
+        
+        
+//         // use filter function
+         
+        
+//         router.post("/post-query-1", function(req,res){
+//         let input =req.query.input
+//         let finalArr =myArr.filter (ele => ele > input)
+//         //let finalArr =[]
+//         //for(i=0;i<myArray.length;i++){
+//         //if(myArr[i] > input ) finalArr.push(myArr[i])}
+//         //}
+//          //let data=req.query
+//         //console.log(data)
+//          res.send(result: finalArr , status :true})
+//          })
+  //customize code
 
-// api fetch all movies
+    //let finalArr =[]
+    //for(i=0;i<myArray.length;i++){
+    //if(myArr[i] > input )
+    // finalArr.push(myArr[i])}
+    //}
+     //let data=req.query
+    //console.log(data)    
+   // res.send(  { data: players , status: true }  )
 
-  router.get('/moviez' ,function(req,res){
-
-    res.send([{id:1,name: 'rock'},{id:2,name: 'rockstar'},{id:3,name: 'Idiot'},{id:4,name: 'puspha'},])
-  });
-
-  // fetch all movie with index
-  router.get('/film/:filmid' , function(req,res){
-let movi =[{id:1,name: 'rock'},{id:2,name: 'rockstar'},{id:3,name: 'Idiot'},{id:4,name: 'puspha'},]
-let value=req.params.filmid;
-let found =false;
-for(i=0;i<movi.length;i++){
-    if(value>movi.length-1){
-        res.send('"Not found"')
+        
+let persons = [
+    {
+        name: "PK",
+        age: 10,
+        votingStatus: false
+    },
+    {
+        name: "SK",
+        age: 20,
+        votingStatus: false
+    },
+    {
+        name: "AA",
+        age: 70,
+        votingStatus: false
+    },
+    {
+        name: "SC",
+        age: 5,
+        votingStatus: false
+    },
+    {
+        name: "HO",
+        age: 40,
+        votingStatus: false
     }
-    else {
-        res.send(movi[value])
+]
+
+router.post("/election", function (req, res) {
+    let votingAge = req.query.votingAge
+
+    let mahir=[];
+    for (let i = 0; i < persons.length; i++) {
+
+        if (persons[i].age > votingAge) {
+        
+            persons[i].votingStatus = true
+            mahir.push(persons[i])
+        }
     }
+if (mahir.length>0)
+{
+    return res.send(mahir)
+   // return res.send("you are eligible")
 }
+else{
+    return res.send(" not eligible ")
+}
+
   })
+
+ 
 
 module.exports = router;
