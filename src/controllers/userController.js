fc5caf988b1486f7c7292409cd87d1977fe26db2
rmashum <r.mashum@gmail.com>
2022-03-04 23:41:36 +0530

    Assigment-session-middleware-intro

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,15 @@
 const UserModel= require("../models/userModel")
+var ip = require("ip");
+let current_datetime = new Date()
+let formatted_date = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds() 
+
+
+const basicCode = async function (req, res){
+    var fullUrl =  req.originalUrl;
+    console.log(formatted_date,  ip.address(), fullUrl)
+    //console.log("hey man, you have reached the handler")
+    res.send({msg: "this is coming controller (handler)"})
+}
 
 const createUser= async function (req, res) {
     let data= req.body
@@ -10,6 +21,13 @@ const getUsersData= async function (req, res) {
     let allUsers= await UserModel.find()
     res.send({msg: allUsers})
 }
+const datalogged = async function (req, res){
+    var fullUrl =  req.originalUrl;
+    console.log(formatted_date,  ip.address(), fullUrl)
+    res.send({msg: "You Are In Profile"})
+}
 
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
+module.exports.basicCode= basicCode
+module.exports.datalogged=datalogged
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,17 +7,27 @@ const BookController= require("../controllers/bookController")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
-
 router.post("/createUser", UserController.createUser  )
-
 router.get("/getUsersData", UserController.getUsersData)
-
 router.post("/createBook", BookController.createBook  )
-
 router.get("/getBooksData", BookController.getBooksData)
-
 router.post("/updateBooks", BookController.updateBooks)
 router.post("/deleteBooks", BookController.deleteBooks)
+
+const mid1 = function (req , res, next){
+    let loggedIn = true
+    if (loggedIn == true){
+        next()  }  else{      res.send ("please register")
+    }}
+router.get("/basicRouter" , mid1 ,UserController.basicCode)
+
+
+const mid2 = function (req , res, next){
+    
+    console.log("Hello from logger" );
+  next()
+};
+router.get("/datalogin" , mid2 ,UserController.datalogged)
 
 //MOMENT JS
 const moment = require('moment');
@@ -29,13 +39,13 @@ router.get("/dateManipulations", function (req, res) {
     // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
     // console.log(validOrNot)
     
-    const dateA = moment('01-01-1900', 'DD-MM-YYYY');
-    const dateB = moment('01-01-2000', 'DD-MM-YYYY');
+    // const dateA = moment('01-01-1900', 'DD-MM-YYYY');
+    // const dateB = moment('01-01-2000', 'DD-MM-YYYY');
 
-    let x= dateB.diff(dateA, "days")
-    console.log(x)
+    // let x= dateB.diff(dateA, "days")
+    // console.log(x)
 
-    res.send({ msg: "all good"})
+    // res.send({ msg: "all good"})
 })
 
 module.exports = router;
